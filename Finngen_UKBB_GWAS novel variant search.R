2894c4eed3646c3e014d3b2b93d32d782e525039
Shabbeer Hassan <44751331+Shabs86@users.noreply.github.com>
2020-12-05 00:16:38 +0200

    Update Finngen_UKBB_GWAS novel variant search.R

--- Finngen_UKBB_GWAS novel variant search.R
+++ Finngen_UKBB_GWAS novel variant search.R
@@ -1,6 +1,7 @@
 # AUTHOR - Shabbeer Hassan
-# Date - 21/10/2020 
+# Start Date - 21/10/2020 
 # Match variants across Finngen and UKBB 
+# Try out CONQUER for cross annotations of variants
 
 # Libraries ---------------------------------------------------------
 
@@ -31,6 +32,30 @@ loadfonts(device = "win")
 options(digits = 2)
 options(scipen = 999)
 
+# Packages needed
+## CONQUER Installation
+install.packages("devtools")
+install.packages("conquer.db_0.1.2.tar.gz", type="source", repos=NULL)
+devtools::install_github("roderickslieker/CONQUER.DB")
+devtools::install_github("roderickslieker/CONQUER.d3")
+devtools::install_github("roderickslieker/CONQUER")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("GenomicRanges")
+install.packages("rjson_0.2.20.tar.gz", type="source", repos=NULL)
+install.packages("enrichR_2.1.tar.gz", type="source", repos=NULL)
+install.packages("shinyjs_2.0.0.tar.gz", type="source", repos=NULL)
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+## PRIMO install
+BiocManager::install("limma")
+install.packages("MASS")
+install.packages("matrixStats")
+install.packages("nnls")
+install.packages("R.methodsS3")
+install.packages("lcmix",repos="http://r-forge.r-project.org")
+devtools::install_github("kjgleason/Primo")
+
 # Input Arguments ---------------------------------------------------------
 
 # Finngen_GWAS
@@ -59,12 +84,10 @@ View(Final_list_Finngen)
 All_sig_vars <- read_excel("Dropbox/Important_Documents/Doctoral_Work/Main Work/Paper 2 - Finngen_varicose veins_DVT/Variant lists etc/Final list_Finngen.xlsx", 
                            sheet = "All signif variants")
 View(All_sig_vars)
-
 Merged_all_sig_vars <- inner_join(All_sig_vars, coloc, 
                     by = c("Pos"))
 Merged_novel_vars <- inner_join(Final_list_Finngen, coloc, 
                                   by = c("Pos"))
-
 uniqie_loci_coloc <- unique(coloc$Locus)
 
 # Output Files
@@ -74,28 +97,19 @@ write.xlsx(Merged_all_sig_vars, "Dropbox/Important_Documents/Doctoral_Work/Main 
 write.xlsx(Merged_novel_vars, "Dropbox/Important_Documents/Doctoral_Work/Main Work/Paper 2 - Finngen_varicose veins_DVT/Variant lists etc/Merged_all_sig_vars.xlsx", sheetName = "Merged_novel_vars", 
            col.names = TRUE, row.names = TRUE, append = TRUE)
 
-## CONQUER Installation
-install.packages("devtools")
-install.packages("conquer.db_0.1.2.tar.gz", type="source", repos=NULL)
-devtools::install_github("roderickslieker/CONQUER.DB")
-devtools::install_github("roderickslieker/CONQUER.d3")
-devtools::install_github("roderickslieker/CONQUER")
-
 
 ## CONQUER example
-
 library(CONQUER)
 DIR = "F:/Dropbox/Important_Documents/Doctoral_Work/Main Work/Paper 2 - Finngen_varicose veins_DVT"
 library(readxl)
 Final_list_Finngen <- read_excel("Important_Documents/Doctoral_Work/Main Work/Paper 2 - Finngen_varicose veins_DVT/Variant lists etc/Final list_Finngen.xlsx", 
                                  sheet = "Novel hits")
-
 #snps <- Final_list_Finngen$RSID
 snps <- c("rs192335285","rs191238524","rs635634","rs139988391","rs201955556") 
-
 snps<-c("rs977371848", "rs992108547","rs947073006", "rs600038", "rs651007", "rs579459", "rs495828")
 
-
+# Summarizeresults across snps
+tissues_list <- conquer.db::gtexTissuesV8
 CONQUER::summarize(variants = snps ,
                    directory=DIR,
                    multiAnalyze=TRUE,
@@ -105,43 +119,10 @@ CONQUER::summarize(variants = snps ,
 #,
 #                   tissues=c("Pancreas","Muscle_Skeletal","Liver"))
 
+# Visualise results via plotly
 visualize(directory = DIR, SNPs = snps)
 
-tissues_list <- conquer.db::gtexTissuesV8
-
-
-
-
-
-
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
-BiocManager::install("GenomicRanges")
-
-
-install.packages("rjson_0.2.20.tar.gz", type="source", repos=NULL)
-
-
-install.packages("enrichR_2.1.tar.gz", type="source", repos=NULL)
-
-
-install.packages("shinyjs_2.0.0.tar.gz", type="source", repos=NULL)
-
-
-
 # PRIMO analysis
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("limma")
-
-install.packages("MASS")
-install.packages("matrixStats")
-install.packages("nnls")
-install.packages("R.methodsS3")
-install.packages("lcmix",repos="http://r-forge.r-project.org")
-
-devtools::install_github("kjgleason/Primo")
 library("Primo")
 
 
@@ -162,8 +143,6 @@ library("Primo")
 
 
 ############################# JUNK ##################################
-
-
 
 # UKBB_GWAS
 UKBB_gwas <- read_excel("Important_Documents/Doctoral_Work/Main Work/Paper 2 - Finngen_varicose veins_DVT/Variant lists etc/GWAS results from UKBB+23&me_2020 paper.xlsx")
@@ -187,8 +166,6 @@ View(Finngen)
 # Comparisons
 UKBB_Finngen_common_3 <- Finngen %>% inner_join(UKBB_published, by=c("pos"))
 a<-merge(UKBB_gwas, Finngen, by=c("pos"))
-
-
 
 library(CONQUER)
 snps <- c("rs11642430")
